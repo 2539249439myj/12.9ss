bf63c03043f9e32379cb78e81b16d24301b2fee3
myj <2539249439@qq.com>
2020-12-09 19:18:01 +0800

    src

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import bg from './index.css';
+import indexLess from './index.less';
+import jp2 from './2.jpg';
+var img2=document.createElement('img');
+    img2.src=jp2;
+    document.body.appendChild(img2);
